405d73b43d9b4737b1e3b516ad16c0c071668237
Ric0 <ariazjean@gmail.com>
2025-11-10 17:30:47 -0400

    routing tools updated and delete implementation added

--- app/utils/marker_tools.ts
+++ app/utils/marker_tools.ts
@@ -10,21 +10,30 @@ export default  class MarkerTools {
     constructor(map: L.Map,tool_kit_map_buttons:Array<{icon:string}> , options?: unknown) {
         this.map = map;
         this.tool_kit_map_buttons = tool_kit_map_buttons;
-
-
         this.tool_click_events(this.map, this.tool_kit_map_buttons);
     }
 
     addMarker(latlng: L.LatLngExpression, options?: L.MarkerOptions): L.Marker {
         // Lógica para añadir un marcador al mapa
         const marker = L.marker(latlng, options).addTo(this.map);
+        this.draw_list.push(marker);
         return marker;
     }
 
     trazarRuta(puntos: L.LatLngExpression[], options?: L.PolylineOptions): L.Polyline {
+
+        console.log(`ruta en coords  a previas ${this.prev_coords}`);
         // Lógica para trazar una ruta en el mapa
-        const polyline = L.polyline(puntos, options).addTo(this.map);
-        return polyline;
+        setTimeout(() => {
+            this.draw_list.push(L.polyline(puntos, options).addTo(this.map));
+        }, 100);
+
+
+    }
+    clearDrawList() {
+        this.draw_list.forEach((item) => {
+            this.map.removeLayer(item);
+        });
     }
 
     tool_click_events(map: L.Map, tool_kit_map_buttons: Array<{icon:string}>) {
@@ -43,7 +52,7 @@ export default  class MarkerTools {
                                 this.route()
                                     break;
                             case 'delete':
-                                    
+                                    this.clearDrawList();
                                     break;
                             
                             default:
@@ -63,32 +72,29 @@ export default  class MarkerTools {
         // if add_location_alt in events, remove it event listener and return
         if (this.events.includes('add_location_alt')) {
             console.log("Remove add_location_alt event listener");
-            this.map.off('click', this.add_location_fun);
             this.events = this.events.filter(event => event !== 'add_location_alt');
-            return;
         }
-        // leaflet set onclick interaction
-        this.map.on('click', this.add_location_fun);
-        this.events.push('add_location_alt');
+        else{ this.events.push('add_location_alt'); }
+
+        this.load_events();
     }
 
     // función para trazar ruta en el mapa
     route() {
         console.log("Route tool activated");
-
+        // if route in events, remove it event listener and return
         if (this.events.includes('route')) {
             console.log("Remove route event listener");
-            this.map.off('click', this.add_route_fun);
             this.events = this.events.filter(event => event !== 'route');
-            return;
+        }
+        else{
+        this.events.push('route');
         }
 
-        // leaflet set onclick interaction
-        this.map.on('click', this.add_route_fun);
-        this.events.push('route');
+        this.load_events();
+
+
     }
-
-
     // toggle style of button
     style_toggle_button(buttonElement: HTMLElement) {
         // Lógica para cambiar el estilo del botón al hacer clic
@@ -106,12 +112,11 @@ export default  class MarkerTools {
             // guardar coordenadas previas para trazar ruta
             this.prev_coords = e.latlng;
           }
-
+    // función para trazar ruta en el mapa
     add_route_fun = (e: L.LeafletMouseEvent) => {
 
         const { lat, lng } = e.latlng;
         // if this.prev_coords is  null return
-        console.log(`ruta en coords ${lat}, ${lng} a previas ${this.prev_coords}`);
         
         if (this.prev_coords == null) { return this.prev_coords = e.latlng;}
 
@@ -123,5 +128,14 @@ export default  class MarkerTools {
         
         // guardar coordenadas previas para trazar ruta
         this.prev_coords = e.latlng;
+    }
+    // función para cargar eventos adicionales si es necesario
+    load_events() {
+        console.log("Load events");
+        // limpiar eventos previos
+        this.map.off('click');
+        // Aquí puedes cargar o inicializar eventos adicionales si es necesario
+        if (this.events.includes('route')) {this.map.on('click', this.add_route_fun);}
+        if (this.events.includes('add_location_alt')) {  this.map.on('click', this.add_location_fun);}   
     }
 }
